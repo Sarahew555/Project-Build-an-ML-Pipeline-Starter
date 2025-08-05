45cb55214a743e17a75e93ce39813161100c840a
Sarahew555 <sarahew555@gmail.com>
2025-08-05 10:06:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -118,8 +118,8 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/test_regression_model",
                 'main',
                 parameters = {
-                    "mlflow_model": "random_forest_export:prod"
-                    "test_dataset": "test_data.csv:latest"
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest",
                 },
             )
 
